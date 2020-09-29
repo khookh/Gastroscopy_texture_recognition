823363694c872ae291b4bb3ef10cc5338a6df5d5
sdonne <stefanodonne@gmail.com>
2020-09-29 13:29:41 +0200

    Add zone contrast

--- bubble_test.py
+++ bubble_test.py
@@ -1,58 +1,50 @@
 import numpy as np
+import skimage
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from skimage.data import camera, astronaut
+import skimage.color
+import skimage.io
+import skimage.viewer
 from IPython.display import HTML, Image, SVG, YouTubeVideo
+import methods
 
-img_gastro = mpimg.imread('bulles.png')
-imgplot = plt.imshow(img_gastro)
+# img_gastro = mpimg.imread('bulles.png')
+# imgplot = plt.imshow(img_gastro)
+# plt.show()
+
+image_gastro = skimage.io.imread('bulles.png')
+viewer = skimage.viewer.ImageViewer(image_gastro)
+# viewer.show()
+
+#display basic hist of the picture
+#methods.display_hist(image_gastro)
+
+#negative trans + hist
+#lut = np.arange(255, -1, -1)
+#methods.apply_lut(image_gastro, lut)
+
+#auto level
+#t_ima = (image_gastro/4+64).astype(np.uint8)
+#methods.display_hist(t_ima,vmin=0,vmax=255)
+#methods.apply_lut(t_ima,methods.lut_autolevel(t_ima))
+
+#equalization
+#methods.apply_lut(image_gastro,methods.lut_equalization(image_gastro))
+
+#equalization in a certain square
+roi = [(300,700),300,200] #around the bubles
+sample = image_gastro[roi[0][1]:roi[0][1]+roi[2],roi[0][0]:roi[0][0]+roi[1]]
+lut = methods.lut_equalization(sample)
+
+plt.figure()
+plt.imshow(image_gastro/255,cmap=cm.gray)
+rect = plt.Rectangle(*roi, facecolor=None,alpha=.25)
+plt.gca().add_patch(rect)
+plt.figure()
+
+plt.imshow(lut[image_gastro]/255,cmap=cm.gray);
+rect = plt.Rectangle(*roi, facecolor=None,alpha=.25)
+plt.gca().add_patch(rect);
 plt.show()
-
-plt.style.use('ggplot')
-
-def norm_hist(ima):
-    hist, bins = np.histogram(ima.flatten(), range(256))  # histogram is computed on a 1D distribution --> flatten()
-    return 1. * hist / np.sum(hist)  # normalized histogram
-
-
-def display_hist(ima, vmin=None, vmax=None):
-    plt.figure(figsize=[10, 5])
-    if ima.ndim == 2:
-        nh = norm_hist(ima)
-    else:
-        nh_r = norm_hist(ima[:, :, 0])
-        nh_g = norm_hist(ima[:, :, 1])
-        nh_b = norm_hist(ima[:, :, 2])
-    # display the results
-    plt.subplot(1, 2, 1)
-    plt.imshow(ima, cmap=cm.gray, vmin=vmin, vmax=vmax)
-    plt.subplot(1, 2, 2)
-    if ima.ndim == 2:
-        plt.plot(nh, label='hist.')
-    else:
-        plt.plot(nh_r, color='r', label='r')
-        plt.plot(nh_g, color='g', label='g')
-        plt.plot(nh_b, color='b', label='b')
-    plt.legend()
-    plt.xlabel('gray level');
-    plt.show()
-
-display_hist(img_gastro)
-
-
-def apply_lut(ima, lut, vmin=None, vmax=None):
-    nh = norm_hist(ima)
-    lima = lut[ima]
-    nh_lima = norm_hist(lima)
-
-    plt.figure(figsize=[10, 5])
-    plt.subplot(1, 2, 1)
-    plt.imshow(lima, cmap=cm.gray, vmin=vmin, vmax=vmax)
-    ax1 = plt.subplot(1, 2, 2)
-    plt.plot(nh, label='ima')
-    plt.plot(nh_lima, label='lut[ima]')
-    plt.legend(loc='upper left')
-    ax2 = ax1.twinx()
-    plt.plot(lut, label='lut', color='k')
-    plt.legend()
