4402d23a1c1477ce6d4008e03ea0058ac4835951
sdonne <stefanodonne@gmail.com>
2020-10-06 21:06:18 +0200

    Fix empty frame bug

--- seg_and_score.py
+++ seg_and_score.py
@@ -5,6 +5,7 @@ import skimage.viewer
 import methods
 import cv2 as cv
 
+
 # renvoie une image segmentée par histogramme
 def seg_his(ima, path):
     # methods.display_hist(ima)
@@ -18,20 +19,22 @@ def seg_his(ima, path):
     # cv.imwrite(path, sel)
     return sel
 
+
 # renvoie un score de qualité à partir de l'image segmentée
 def score(ima, dim):
-    liste_defauts = cv.findNonZero(ima)
-    # prototype
-    dmax = np.math.sqrt(dim[1] * dim[1] + dim[0] * dim[0])
     score = 0
-    if liste_defauts !=  None:
+    liste_defauts = cv.findNonZero(ima)
+    if liste_defauts is not None :
+        if len(liste_defauts) > dim[0] * dim[1] * 0.2:
+            return 15
+        # prototype
+        dmax = np.math.sqrt(dim[1] * dim[1] + dim[0] * dim[0])
+        score = 0
         for elem in liste_defauts:
             difx = elem[0][0] - dim[0]
             dify = elem[0][1] - dim[1]
             dnorm = (np.math.sqrt(difx * difx + dify * dify)) / dmax
-            if dnorm > 1:
-                print(dnorm)
-            score += (1.7 / 5000) * (1 - pow(dnorm,3)) #coef arbitraire basé sur l'exp
+            score += (70 / (dim[0] * dim[1])) * (1 - pow(dnorm, 3))  # coef arbitraire basé sur l'exp
         if score > 15:
             score = 15
         print(score)
@@ -56,22 +59,24 @@ while cap.isOpened():
     # cv.imshow('window-name', frame)
     # cv.imwrite("frames/frame%d.png" % count, frame)
     # segmentation
-    frame2gray = skimage.util.img_as_ubyte(skimage.color.rgb2gray(frame))
-    ret = seg_his(frame2gray, "frames/frame%d.png" % count)
-    # score
-    sco= score(ret, dim)
-    # resize pour affichage propre
-    ret = cv.resize(ret, None, fx=0.4, fy=0.4, interpolation=cv.INTER_AREA)
-    frame = cv.resize(frame, None, fx=0.4, fy=0.4, interpolation=cv.INTER_AREA)
-    # concatene les deux images pour comparaison
-    numpy_h_concat = np.hstack((frame, skimage.color.gray2rgb(ret)))
-    # add score + n frame à l'image
-    image = cv.putText(numpy_h_concat, 'Score = %d' % sco, (5, 400), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 1,
-                       cv.LINE_AA)
-    image = cv.putText(image, 'Frame %d' % count, (5, 370), cv.FONT_HERSHEY_SIMPLEX, .4, (0, 0, 255), 1,
-                       cv.LINE_AA)
-    # show dans la fenêtre
-    cv.imshow('comparison', image)
+    if frame is not None:
+        frame2gray = skimage.util.img_as_ubyte(skimage.color.rgb2gray(frame))
+        ret = seg_his(frame2gray, "frames/frame%d.png" % count)
+        # score
+        sco = round(score(ret, dim))
+        # resize pour affichage propre
+        ret = skimage.color.gray2rgb(ret)
+        ret = cv.resize(ret, None, fx=0.4, fy=0.4, interpolation=cv.INTER_AREA)
+        frame = cv.resize(frame, None, fx=0.4, fy=0.4, interpolation=cv.INTER_AREA)
+        # concatene les deux images pour comparaison
+        numpy_h_concat = np.hstack((frame, ret))
+        # add score + n frame à l'image
+        image = cv.putText(numpy_h_concat, 'Score = %d' % sco, (5, 400), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 1,
+                           cv.LINE_AA)
+        image = cv.putText(image, 'Frame %d' % count, (5, 370), cv.FONT_HERSHEY_SIMPLEX, .4, (0, 0, 255), 1,
+                           cv.LINE_AA)
+        # show dans la fenêtre
+        cv.imshow('comparison', image)
     if cv.waitKey(5) & 0xFF == ord('p'):
         while True:
             if cv.waitKey(5) & 0xFF == ord('s'):
