c875491eeb413844a34ef6c3cf24628f9040bb40
sdonne <stefanodonne@gmail.com>
2021-01-27 12:51:43 +0100

    Improve framerate drastically

--- seg_and_score.py
+++ seg_and_score.py
@@ -15,11 +15,10 @@ import os
 def seg_hsv(img):
     img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     h, s, v = cv.split(img)
-    mean_sat = np.mean(np.ndarray.flatten(s))
     # temp seg masks
     mask = cv.inRange(img, (0, 35, 170), (60, 100, 245))  # direct light
     mask2 = cv.inRange(img, (0, 0, 90), (30, 95, 170))  # low light foam
-    return mask + mask2, mean_sat
+    return mask + mask2
     # return cv.bitwise_and(img, img, mask=mask)
 
 
@@ -32,18 +31,28 @@ def score(ima, _dim):
     return score
 
 
+kernel = np.ones((9, 9), np.uint8)
+kernelb = np.ones((3, 3), np.uint8)
+
+
 # applique les transpho morphologiques à l'image
 def morph_trans(ima):
-    kernel = np.ones((9, 9), np.uint8)
+    global kernel, kernelb
     ima = cv.morphologyEx(ima, cv.MORPH_CLOSE, kernel)  # clustering
-    ima = cv.morphologyEx(ima, cv.MORPH_OPEN, np.ones((3, 3), np.uint8))  # denoise
+    ima = cv.morphologyEx(ima, cv.MORPH_OPEN, kernelb)  # denoise
     ima = cv.morphologyEx(ima, cv.MORPH_OPEN, kernel)  # denoise
     # ima = cv.dilate(ima, np.ones((5, 5), np.uint8), iterations=1)
     return ima
 
+# returns the uniformity of the image
+def uniformity(ima):
+    blur1_uni = cv.GaussianBlur(ima, (7, 7), 1)
+    blur2_uni = cv.GaussianBlur(ima, (31, 31), 2)
+    return np.sum((blur1_uni - blur2_uni) ** 2)
+
 
 def strict_diff():
-    global blur_list, count
+    global blur_list
     if blur_list.size > 4:
         if blur_list[-1] != blur_list[-2] and blur_list[-2] != blur_list[-3] and blur_list[-3] != blur_list[-4]:
             return True
@@ -51,13 +60,14 @@ def strict_diff():
 
 
 def strict_eq():
-    global blur_list, count
+    global blur_list
     if blur_list.size > 4:
         if blur_list[-1] == blur_list[-2] and blur_list[-2] == blur_list[-3] and blur_list[-3] == blur_list[-4]:
             return True
     return False
 
 
+# output into thefile the score of the section that has been processed
 def section_score():
     global f, score_list, section_score_list, section
     f.write("Mean score in section %i = %.2f \n" % (section, np.mean(section_score_list)))
@@ -67,6 +77,7 @@ def section_score():
     section += 1
 
 
+# save the temporary score buffer into the section list
 def save():
     global section_score_list, temp_score_list
     if temp_score_list.size > 8:
@@ -76,7 +87,7 @@ def save():
 
 # lecture flux vidéo
 section, count = 1, 1
-mean_s, sco, blur = 0, 0, 0
+sco,  unfy = 0, 0
 p_capture = False
 over = False
 blur_list = np.array([])
@@ -88,35 +99,35 @@ q_treated = queue.Queue()
 f = open("output_%s.txt" % os.path.basename(str(sys.argv[1])), "w")
 
 
+# Thread reading the video flux
 def read_flux():
-    global count,  over
+    global count, over
     cap = cv.VideoCapture(str(sys.argv[1]))
     while not cap.isOpened():  # attente active en cas de lecture de flux en real-time, on attend le header
         cap = cv.VideoCapture(str(sys.argv[1]))
         cv.waitKey(500)
     while cap.isOpened():
-        print("r")
         while q_frame.qsize() > 50:
             time.sleep(0)
         retr, frame = cap.read()
-        q_frame.put(frame)
         if retr is not True and count > 1:
             cap.release()
             over = True
             break
-        elif retr is True:
+        if retr is True:
+            q_frame.put(cv.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv.INTER_CUBIC))
             count += 1
         if over is True:
             cap.release()
             break
 
 
+# thread treating the frames
 def frame_treatment():
-    global temp_score_list, section_score_list, score_list, blur_list, count, section, p_capture, blur, mean_s
+    global temp_score_list, section_score_list, score_list, blur_list, count, section, p_capture, unfy
     local_count = 1
     while True:
-        print("t")
-        while q_frame.empty():
+        if q_frame.empty():
             time.sleep(0)
         frame = q_frame.get()
         if local_count == 1:
@@ -124,44 +135,45 @@ def frame_treatment():
             centrex, centrey = dimensions[1] / 2, dimensions[0] / 2
             dim = (int(centrex), int(centrey))
             frame_treated = np.zeros(dimensions)
-        # motion blur level
-        blur = cv.Laplacian(frame, cv.CV_32F).var()
-        blur_list = np.append(blur_list, round(blur))
+        # uniformity
+        unfy = uniformity(frame) /(dim[0]*dim[1]*4)
+        blur_list = np.append(blur_list, unfy)
         if strict_eq():
             p_capture = True
+            save()
         if p_capture is True and strict_diff():
             p_capture = False
             section_score()
-        if blur < 1200:
-            frame_treated, mean_s = seg_hsv(frame)
-            if mean_s < 130:  # np.mean(mean_sv) + 50:
-                frame_treated = morph_trans(frame_treated)
-                temp_score_list = np.append(temp_score_list, round(score(frame_treated, dim) * 100, 3))
-            else:
-                save()
+            temp_score_list = np.array([])
+
+        if unfy > 15:  # np.mean(mean_sv) + 50:
+            frame_treated = seg_hsv(frame)
+            frame_treated = morph_trans(frame_treated)
+            temp_score_list = np.append(temp_score_list, round(score(frame_treated, dim) * 100, 3))
         else:
             save()
+
         if over is True:
             save()
             section_score()
             break
-        q_treated.put(frame)
+        q_treated.put(frame)#(frame,unfy))
         local_count += 1
 
 
+# Thread displaying the frames
 def display_t():
-    global score_list, count, over
+    global section_score_list, score_list, count, over
     local_count = 1
     while True:
-        print("d")
-        while q_treated.empty():
+        if q_treated.empty():
             time.sleep(0)
-        frame = q_treated.get()
+        frame = q_treated.get()#[0]
         # Affichage
         frame = skimage.color.gray2rgb(frame)
         # resize pour affichage propre
         # frame_treated_f = cv.resize(frame_treated_f, None, fx=0.4, fy=0.4, interpolation=cv.INTER_AREA)
-        frame = cv.resize(frame, None, fx=0.4, fy=0.4, interpolation=cv.INTER_AREA)
+        # frame = cv.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv.INTER_CUBIC)
         # concatene les deux images pour comparaison
         # numpy_h_concat = np.hstack((frame, frame_treated_f))
         # rajoute les paramètres informatifs
@@ -172,15 +184,15 @@ def display_t():
                            cv.FONT_HERSHEY_SIMPLEX, .5,
                            (0, 0, 255), 1,
                            cv.LINE_AA)
-        # image = cv.putText(image, 'msat = %d' % round(mean_s, 3), (5, 420), cv.FONT_HERSHEY_SIMPLEX, .5,
+        #image = cv.putText(image, 'uniformity = %d' % round(q_treated.get()[1]), (5, 420), cv.FONT_HERSHEY_SIMPLEX, .5,
         #                   (0, 0, 255), 1,
         #                   cv.LINE_AA)
-        # image = cv.putText(image, 'blur = %d' % round(blur), (5, 350), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 1,
+        #image = cv.putText(image, 'blur = %d' % round(q_treated.get()[1]), (5, 350), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 1,
         #                   cv.LINE_AA)
         # show dans la fenêtre
         cv.imshow('comparison', image)
-        local_count+=1
-        # cv.imwrite('frames/frame%d.png' % count, image)
+        local_count += 1
+        cv.imwrite('frames/resizeLINEAR%d.png' % local_count, image)
         k = cv.waitKey(1) & 0xFF
         if k == ord('p'):
             while True:
